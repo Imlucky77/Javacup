5fa1e0dd40bf7c14e30abc640c60ea94c31116ce
imlucky <ali.dabagh7@gmail .com>
2019-01-20 15:58:14 +0330

    Exercises Season Two

--- src/howtoprogram/seasontwo/SeparatingDigits.java
+++ src/howtoprogram/seasontwo/SeparatingDigits.java
@@ -1,6 +1,5 @@
 package howtoprogram.seasontwo;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -11,9 +10,14 @@ public class SeparatingDigits {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter a number: ");
-        int sca = input.nextInt();
-        System.out.println();
+        int number = input.nextInt();
 
+        int digit5 = number % 10;
+        int digit4 = (number % 100) / 10;
+        int digit3 = (number % 1000) / 100;
+        int digit2 = (number % 10000) / 1000;
+        int digit1 = number / 10000;
 
+        System.out.printf("%d   %d   %d   %d   %d   ", digit1, digit2, digit3, digit4, digit5);
     }
 }
