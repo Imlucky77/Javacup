7e807a6aa11e2aae4bd3c667d2c1639feef6f32c
imlucky <ali.dabagh7@gmail .com>
2019-01-23 16:19:29 +0330

    Exercises Season Three

---
+++ src/howtoprogram/seasonthree/exercise_13/EmployeeTest.java
@@ -0,0 +1,30 @@
+package howtoprogram.seasonthree.exercise_13;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee james = new Employee("james", "gasling", 3);
+        Employee bill = new Employee("bill", "gates", 5);
+
+        double jamesYearlySalary = james.getSalary() * 12;
+        double billYearlySalary = bill.getSalary() * 12;
+
+
+        System.out.println("James's annual salary is: " + jamesYearlySalary);
+        System.out.println("bill's annual salary is: " + billYearlySalary);
+        System.out.println();
+
+        double jamesRaiseRate = (jamesYearlySalary / 100) * 10;
+        System.out.println("james Raise Rate is: " + jamesRaiseRate);
+        double billRaiseRate = (billYearlySalary / 100) * 10;
+        System.out.println("bill Raise Rate is: " + billRaiseRate);
+
+        double jamesTotalSalary = jamesYearlySalary + jamesRaiseRate;
+        double billTotalSalary = billYearlySalary + billRaiseRate;
+        System.out.println();
+
+        System.out.println("James's annual salary after 10% raise is: " + jamesTotalSalary);
+        System.out.println("bill's annual salary after 10% raise is: " + billTotalSalary);
+
+
+    }
+}
---
+++ src/howtoprogram/seasonthree/exercise_14/DateTest.java
@@ -0,0 +1,9 @@
+package howtoprogram.seasonthree.exercise_14;
+
+public class DateTest {
+    public static void main(String[] args) {
+        Date date = new Date(23, 01, 2019);
+
+        date.displayDate();
+    }
+}
