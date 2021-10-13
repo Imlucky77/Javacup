6d76f1fc50a62828854186a7f957b4905e2f881e
ali <ali.dabagh7@gmail.com>
2021-10-13 11:06:29 +0330

    Add java test programming

---
+++ src/main/java/basicjava/javase/ValueOf.java
@@ -0,0 +1,9 @@
+package basicjava.javase;
+
+public class ValueOf {
+    public static void main(String[] args) {
+        Integer value = Integer.valueOf("444", 16);
+        System.out.println(value);
+
+    }
+}
