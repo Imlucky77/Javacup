7634d3b19951398fe8d1a9e3f62cfe530db6bd12
ali <ali.dabagh7@gmail.com>
2021-10-27 11:49:27 +0330

    exercise for cherry-pick command try two

--- src/main/java/java8/TestCherry.java
+++ src/main/java/java8/TestCherry.java
@@ -3,5 +3,6 @@ package java8;
 public class TestCherry {
     public static void main(String[] args) {
         System.out.println("Cherry-pick is done!");
+        System.out.println("Definitely I can!");
     }
 }
